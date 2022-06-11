f1ea0abd47604abcaea0842820cdc690ef6b21fc
kostya-alch <kostafrompiter@gmail.com>
2022-06-11 17:45:22 +0300

    rendering list with map, active class toggle

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react"
 
 
 export const Categories = () => {
+  const [activeCategory, setActiveCategory] = useState(0)
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {
+          categories.map((category, index) =>
+            <li onClick={() => setActiveCategory(index)} key={category} className={activeCategory === index ? 'active' : ''}>
+              {category}
+            </li>
+          )
+        }
       </ul>
     </div>
   )
